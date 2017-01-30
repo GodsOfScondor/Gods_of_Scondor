6616a6c822567749122c2ee001e7e5316ce79b0a
Thomas Langs <Thomas Langs@DESKTOP-JPM23JL>
2017-01-30 21:18:14 +0100

    test 3.0

--- src/scondor/deck/card/Card.java
+++ src/scondor/deck/card/Card.java
@@ -1,5 +1,19 @@
 package scondor.deck.card;
 
-public class Card {
+public abstract class Card {
+	
+	private int mana_cost;
+	
+	public Card(int mana_cost){
+		this.mana_cost = mana_cost;
+	}
+	
+	public int getManaCost(){
+		return mana_cost;
+	}
 
+	public void setManaCost(int mana_cost){
+		this.mana_cost = mana_cost;
+	}
+	
 }
