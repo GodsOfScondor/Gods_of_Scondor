84cd32fff86ecf7e15e044e1ae4568fd0c7fdaf0
Andreas <Andreas@DESKTOP-1BV1L6L>
2017-03-28 19:07:10 +0200

    fixed bernis bug that only occured on baldingas pc

--- src/scondor/content/Connection.java
+++ src/scondor/content/Connection.java
@@ -31,9 +31,9 @@ public class Connection {
 	private String password;
 	private String license;
 	private String line;
-	
+
 	private Contents contents;
-	
+
 	private final int USER_ALREADY_ONLINE = 0;
 	private final int WRONG_PASSWORD = 1;
 	private final int LOGIN_SUCCESFULL = 2;
@@ -46,21 +46,27 @@ public class Connection {
 			file = new File(PATH);
 
 			if (file.exists()) {
-				
+
 				reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
 
 				while ((line = reader.readLine()) != null) {
-					if (line.startsWith("save=")) save = Boolean.parseBoolean(line.split("=")[1]);
-					if (line.startsWith("license=")) license = line.split("=")[1];
-					if (line.startsWith("name=")) username = line.split("=")[1];
-					if (line.startsWith("password=")) password = line.split("=")[1];
+					if (line.startsWith("save="))
+						save = Boolean.parseBoolean(line.split("=")[1]);
+					if (line.startsWith("license="))
+						license = line.split("=")[1];
+					if (line.startsWith("name="))
+						username = line.split("=")[1];
+					if (line.startsWith("password="))
+						password = line.split("=")[1];
 				}
 
-				if (save) Client.send(new Authentication(username, password));
-				else Panels.show(Panels.LOGIN);
+				if (save)
+					Client.send(new Authentication(username, password));
+				else
+					Panels.show(Panels.LOGIN);
 
 				reader.close();
-				
+
 			} else {
 
 				Panels.show(Panels.REGISTER);
@@ -75,16 +81,20 @@ public class Connection {
 	public String msgFromServer(int code) {
 
 		switch (code) {
-		case USER_ALREADY_ONLINE: return Messanger.build("User is already online!", 1, 0, 0);
-		case WRONG_PASSWORD: return Messanger.build("Wrong Password!", Panels.LOGIN, 1, 0, 0);
+		case USER_ALREADY_ONLINE:
+			return Messanger.build("User is already online!", 1, 0, 0);
+		case WRONG_PASSWORD:
+			return Messanger.build("Wrong Password!", Panels.LOGIN, 1, 0, 0);
 		case LOGIN_SUCCESFULL:
 			saveData();
 			contents = new Contents();
 			contents.load();
-			int panel = contents.getAvaibleCards().size()==0?Panels.DECK_STARTER:Panels.MAIN;
+			int panel = contents.getAvaibleCards().size() == 0 ? Panels.DECK_STARTER : Panels.MAIN;
 			return Messanger.build("Login succesfully!", panel, 0, 1, 0);
-		case WRONG_LICENSE: return Messanger.build("License is wrong!", 1, 0, 0);
-		case USERNAME_ALREADY_EXISTS: return Messanger.build("Username already exists!", 1, 0, 0);
+		case WRONG_LICENSE:
+			return Messanger.build("License is wrong!", 1, 0, 0);
+		case USERNAME_ALREADY_EXISTS:
+			return Messanger.build("Username already exists!", 1, 0, 0);
 		}
 
 		return "nopopup";
@@ -97,43 +107,47 @@ public class Connection {
 
 		Client.send(new Verification(username, password, license));
 	}
-	
+
 	public void login(String username, String password) {
 		this.username = username;
 		this.password = password;
-		
+
 		Client.send(new Authentication(username, password));
 	}
-	
+
 	private void saveData() {
 
 		try {
-			
+
 			file = new File(PATH);
-			
-			if (file.exists()==false) file.createNewFile();
-			
+
+			if (!file.exists()) {
+				file.getParentFile().mkdirs();
+				file.createNewFile();
+			}
+
 			if (!save) {
-			
-			writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF-8"));
-			
-			
-				
-				writer.write("save="+(save_data||save)); writer.newLine();
-				
+
+				writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF-8"));
+
+				writer.write("save=" + (save_data || save));
+				writer.newLine();
+
 				if (save_data) {
-					writer.write("name=" + username); writer.newLine();
-					writer.write("password=" + password); writer.newLine();
+					writer.write("name=" + username);
+					writer.newLine();
+					writer.write("password=" + password);
+					writer.newLine();
 				}
-				
+
 				writer.write("license=" + license);
 
-			writer.close();
-			
+				writer.close();
+
 			} else {
-				
+
 				// TODO switch user
-				
+
 			}
 
 		} catch (IOException e) {
@@ -141,19 +155,20 @@ public class Connection {
 		}
 
 	}
-	
+
 	public void incoming(Packet packet) {
 		if (packet instanceof CardList) {
-			contents.incoming((CardList)packet);
+			contents.incoming((CardList) packet);
 		} else if (packet instanceof DeckList) {
-			contents.incoming((DeckList)packet);
+			contents.incoming((DeckList) packet);
 		}
 	}
 
 	public void close() {
-		if (contents!=null) contents.close();
+		if (contents != null)
+			contents.close();
 	}
-	
+
 	public Contents getContents() {
 		return contents;
 	}
